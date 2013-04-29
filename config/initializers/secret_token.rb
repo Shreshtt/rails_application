bf1acbdf4e576681070b37ff7e0d403b382fbcca
karthick <karthickbe2008@gmail.com>
2013-04-29 12:13:49 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ginfy::Application.config.secret_token = '2f85198428988992dec3b0efd3261af80714a1f2db6877321413fb3410c44e349f91e439990ce048f7f802180a648cb9ebc6f9f06fe32076ae3bcc86822eefd6'
